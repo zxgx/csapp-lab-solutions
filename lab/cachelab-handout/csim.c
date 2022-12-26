2fbc524b9fe67815ebfb39d179f9d36d6d98b4ba
zxgx <zhangg1998@outlook.com>
2022-12-26 20:56:01 +0800

    finish lab 5: cache lab

--- lab/cachelab-handout/csim.c
+++ lab/cachelab-handout/csim.c
@@ -1,7 +1,250 @@
+#define MAX_BUF 257
+#define WORD_SIZE 64
+
 #include "cachelab.h"
 
-int main()
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+unsigned setSize=0, numLine=0, blockSize=0;
+unsigned numSize, numBlock;
+char verbose = 0;
+FILE * traceFileFP=NULL;
+
+char buf[MAX_BUF];
+unsigned numHit=0, numMiss=0, numEviction=0;
+
+// cache[i][j][63:63-tagSize]   => tag
+// cache[i][j][0]               => valid bit
+size_t ** cache;
+unsigned ** counter;  // for LRU eviction policy
+size_t tagMask; // extract tag from address
+size_t setMask; // extract set from address
+// omit block bit for Part A
+// size_t blockMask;
+
+
+void printHelpMessage(){
+    printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+    printf("Options:\n");
+    printf("  -h\t\tPrint this help message.\n");
+    printf("  -v\t\tOptional verbose flag.\n");
+    printf("  -s <num>\tNumber of set index bits.\n");
+    printf("  -E <num>\tNumber of lines per set.\n");
+    printf("  -b <num>\tNumber of block offset bits.\n");
+    printf("  -t <file>\tTrace file.\n\n");
+
+    printf("Examples:\n");
+    printf("  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+    printf("  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+}
+
+void checkArgs(char * traceFile){
+    if(setSize==0 || numLine==0 || blockSize==0 || traceFile==NULL){
+        printf("./csm: Missing required command line argument\n");
+        printHelpMessage();
+        exit(0);
+    }
+
+    traceFileFP = fopen(traceFile, "r");
+    if(!traceFileFP){
+        printf("%s: No such file or directory\n", traceFile);
+        exit(0);
+    }
+}
+
+void parseArgs(int argc, char * argv[]){
+    enum argType {
+        NONE, NSET, NLine, NBLOCK, TFILE
+    };
+
+    char * traceFile=NULL;
+    int i=1;
+    enum argType argType=NONE;
+
+    while(i<argc){
+        if(!strcmp(argv[i], "-s")){
+            argType = NSET;
+        } else if(!strcmp(argv[i], "-E")){
+            argType = NLine;
+        } else if(!strcmp(argv[i], "-b")){
+            argType = NBLOCK;
+        } else if(!strcmp(argv[i], "-t")){
+            argType = TFILE;
+        } else if(!strcmp(argv[i], "-v")){
+            verbose = 1;
+        } else if (!strcmp(argv[i], "-h")) {
+            printHelpMessage();
+            exit(0);
+        } else {
+            switch (argType)
+            {
+            case NONE:
+                printf("./csim: invalid option -- %s\n", argv[i]);
+                printHelpMessage();
+                exit(0);
+            case NSET:
+                sscanf(argv[i], "%u", &setSize);
+                argType = NONE;
+                break;
+
+            case NLine:
+                sscanf(argv[i], "%u", &numLine);
+                argType = NONE;
+                break;
+
+            case NBLOCK:
+                sscanf(argv[i], "%u", &blockSize);
+                argType = NONE;
+                break;
+            
+            case TFILE:
+                traceFile = argv[i];
+                argType = NONE;
+                break;
+            }
+        }
+        i++;
+    }
+    checkArgs(traceFile);
+}
+
+void initCache(){
+    unsigned i, j;
+
+    numSize = 1<<setSize;
+    numBlock = 1<<blockSize;
+
+    cache = malloc(sizeof(void *) * (numSize));
+    counter = malloc(sizeof(void *) * (numSize));
+    for(i=0;i<numSize; i++){
+        cache[i] = malloc(sizeof(size_t) * numLine);
+        counter[i] = malloc(sizeof(unsigned) * numLine);
+        for(j=0; j<numLine; j++){
+            cache[i][j] = 0;
+            counter[i][j] = 0;
+        }
+    }
+
+    tagMask = ~((1<<(setSize + blockSize)) - 1);
+    setMask = ~(((1<<blockSize) - 1) | tagMask);
+}
+
+void extractSetAndTag(size_t address, size_t * setPtr, size_t * tagPtr){
+    *setPtr = (address & setMask)>>blockSize;
+    *tagPtr = address & tagMask;
+}
+
+unsigned cacheLoad(size_t set, size_t tag){
+    size_t * line = cache[set], block;
+    unsigned i, replaceBlock = numLine;
+    
+    for(i=0; i<numLine; i++){
+        block = line[i];
+        if((block & 0x1) && !((block&tagMask)^tag)){
+            // hit
+            strcat(buf, " hit");
+            numHit++;
+            return i;
+        }
+
+        if(replaceBlock == numLine && !(block & 0x1))
+            replaceBlock = i;
+    }
+
+    strcat(buf, " miss");
+    numMiss++;
+
+    if(replaceBlock == numLine) {
+        // evict
+        strcat(buf, " eviction");
+        numEviction++;
+
+        unsigned * setCounter = counter[set];
+        unsigned cnt = 0, cur_cnt;
+        for(i=0; i<numLine; i++){
+            block = line[i];
+            cur_cnt = setCounter[i];
+            if(cur_cnt > cnt){
+                replaceBlock = i;
+                cnt = cur_cnt;
+            }
+        }
+    }
+
+    // allocate
+    line[replaceBlock] = tag | 0x1;
+
+    return replaceBlock;
+}
+
+unsigned cacheModify(size_t set, size_t tag){
+    unsigned replaceBlock = cacheLoad(set, tag);
+
+    strcat(buf, " hit");
+    numHit++;
+    
+    return replaceBlock;
+}
+
+void updateCounter(unsigned blockId, size_t set){
+    unsigned * setCounter = counter[set], i;
+    size_t * line = cache[set];
+
+    for(i=0; i<numLine; i++){
+        if(i == blockId){
+            setCounter[i] = 1;
+        } else if((line[i]&0x1)){
+            setCounter[i]++; 
+        }
+    }
+}
+
+int main(int argc, char * argv[])
 {
-    printSummary(0, 0, 0);
+    if(sizeof(size_t)<<3!=WORD_SIZE){
+        printf("This program supposes that the size of size_t is 64 bits.\nWhile this host doesn't hold this assumption.\n");
+        printf("sizeof(size_t)=%ld\n", sizeof(size_t));
+        exit(0);
+    }
+    char operation;
+    size_t address;
+    unsigned size;
+
+    size_t tag, set;
+    unsigned replaceBlock;
+
+    parseArgs(argc, argv);
+    // printf("set size: %u, num line: %u, block size: %u\n", setSize, numLine, blockSize);
+    initCache();
+
+    while (fgets(buf, MAX_BUF-1, traceFileFP)){
+        if(buf[0] == 'I' || sscanf(buf, " %c %lx,%u\n", &operation, &address, &size)!=3)
+            continue;
+
+        extractSetAndTag(address, &set, &tag);
+        // printf("address: %lx ==> extracted set: %lx, tag: %lx\n", address, set, tag);
+        sprintf(buf, "%c %lx,%u", operation, address, size);
+        switch (operation)
+        {
+        case 'L':
+        case 'S':
+            replaceBlock = cacheLoad(set, tag);
+            break;
+        
+        case 'M':
+            replaceBlock = cacheModify(set, tag);
+            break;
+
+        default:
+            printf("unsupport operation: %c", operation);
+            exit(0);
+        }
+        updateCounter(replaceBlock, set);
+        if(verbose) printf("%s\n", buf);
+    }
+    printSummary(numHit, numMiss, numEviction);
+    fclose(traceFileFP);
     return 0;
 }
--- lab/cachelab-handout/trans.c
+++ lab/cachelab-handout/trans.c
@@ -22,6 +22,14 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    int i, j, tmp;
+
+    for (i = 0; i < N; i++) {
+        for (j = 0; j < M; j++) {
+            tmp = A[i][j];
+            B[j][i] = tmp;
+        }
+    }  
 }
 
 /* 
@@ -46,6 +54,253 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 
 }
 
+char transpose_32x32_desc[] = "perform transpose on 8x8 block with 8-elem loop unrolling for 32x32 matrix";
+void transpose_32x32(int M, int N, int A[N][M], int B[M][N]){
+    int i, j, k;
+    int tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7, tmp8;
+
+    for(i=0; i<N; i+=8){
+        for(j=0; j<M-7; j+=8){
+
+            for(k=i; k<i+8&&k<N; k++){
+                tmp1 = A[k][j];
+                tmp2 = A[k][j+1];
+                tmp3 = A[k][j+2];
+                tmp4 = A[k][j+3];
+                tmp5 = A[k][j+4];
+                tmp6 = A[k][j+5];
+                tmp7 = A[k][j+6];
+                tmp8 = A[k][j+7];
+
+                B[j][k] = tmp1;
+                B[j+1][k] = tmp2;
+                B[j+2][k] = tmp3;
+                B[j+3][k] = tmp4;
+                B[j+4][k] = tmp5;
+                B[j+5][k] = tmp6;
+                B[j+6][k] = tmp7;
+                B[j+7][k] = tmp8;
+            }
+        }
+
+        for(; j<M; j++){
+            for(k=i; k<i+8 && k<N; k++)
+                B[j][k] = A[k][j];
+        }
+    }
+}
+
+char transpose_64x64_desc[] = "perform transpose for 64x64 matrix";
+void transpose_64x64(int M, int N, int A[N][M], int B[M][N]){
+    int i, j, k;
+    int tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7, tmp8;
+
+    for(i=0; i<N-7; i+=8){
+        for(j=0; j<M-7; j+=8){
+
+            // copy and transpose the first 4 rows of A to the first 4 rows of B
+            for(k=0; k<4; k++){
+                tmp1 = A[i+k][j];           // miss
+                tmp2 = A[i+k][j+1];
+                tmp3 = A[i+k][j+2];
+                tmp4 = A[i+k][j+3];
+                tmp5 = A[i+k][j+4];
+                tmp6 = A[i+k][j+5];
+                tmp7 = A[i+k][j+6];
+                tmp8 = A[i+k][j+7];
+
+                B[j][i+k] = tmp1;         // miss
+                B[j+1][i+k] = tmp2;       //  |
+                B[j+2][i+k] = tmp3;       //  |
+                B[j+3][i+k] = tmp4;       // miss
+                
+                B[j][i+4+k] = tmp5;
+                B[j+1][i+4+k] = tmp6;
+                B[j+2][i+4+k] = tmp7;
+                B[j+3][i+4+k] = tmp8;
+            }
+            
+            if(i!=j){  // not diagonal
+                for(k=0; k<4; k++){
+                    tmp1 = A[i+4][j+k];     // miss
+                    tmp2 = A[i+5][j+k];     //  |
+                    tmp3 = A[i+6][j+k];     //  |
+                    tmp4 = A[i+7][j+k];     // miss
+
+                    tmp5 = B[j+k][i+4];
+                    tmp6 = B[j+k][i+5];
+                    tmp7 = B[j+k][i+6];
+                    tmp8 = B[j+k][i+7];
+
+                    B[j+k][i+4] = tmp1;
+                    B[j+k][i+5] = tmp2;
+                    B[j+k][i+6] = tmp3;
+                    B[j+k][i+7] = tmp4;
+
+                    B[j+4+k][i] = tmp5;     // miss
+                    B[j+4+k][i+1] = tmp6;
+                    B[j+4+k][i+2] = tmp7;
+                    B[j+4+k][i+3] = tmp8;
+                }
+
+                for(k=0; k<4; k++){
+                    tmp1 = A[i+4+k][j+4];
+                    tmp2 = A[i+4+k][j+5];
+                    tmp3 = A[i+4+k][j+6];
+                    tmp4 = A[i+4+k][j+7];
+
+                    B[j+4][i+4+k] = tmp1;
+                    B[j+5][i+4+k] = tmp2;
+                    B[j+6][i+4+k] = tmp3;
+                    B[j+7][i+4+k] = tmp4;
+                }
+
+            } else {
+                for(k=0; k<4; k++){
+                    tmp1 = A[i+4+k][j];
+                    tmp2 = A[i+4+k][j+1];
+                    tmp3 = A[i+4+k][j+2];
+                    tmp4 = A[i+4+k][j+3];
+                    tmp5 = A[i+4+k][j+4];
+                    tmp6 = A[i+4+k][j+5];
+                    tmp7 = A[i+4+k][j+6];
+                    tmp8 = A[i+4+k][j+7];
+
+                    B[j+4][i+k] = tmp1;
+                    B[j+5][i+k] = tmp2;
+                    B[j+6][i+k] = tmp3;
+                    B[j+7][i+k] = tmp4;
+
+                    B[j+4][i+4+k] = tmp5;
+                    B[j+5][i+4+k] = tmp6;
+                    B[j+6][i+4+k] = tmp7;
+                    B[j+7][i+4+k] = tmp8;
+                }
+
+                for(k=0; k<4; k++){
+                    tmp1 = B[j+4+k][i];
+                    tmp2 = B[j+4+k][i+1];
+                    tmp3 = B[j+4+k][i+2];
+                    tmp4 = B[j+4+k][i+3];
+                    
+                    tmp5 = B[j+k][i+4];
+                    tmp6 = B[j+k][i+5];
+                    tmp7 = B[j+k][i+6];
+                    tmp8 = B[j+k][i+7];
+
+                    B[j+k][i+4] = tmp1;
+                    B[j+k][i+5] = tmp2;
+                    B[j+k][i+6] = tmp3;
+                    B[j+k][i+7] = tmp4;
+
+                    B[j+4+k][i] = tmp5;
+                    B[j+4+k][i+1] = tmp6;
+                    B[j+4+k][i+2] = tmp7;
+                    B[j+4+k][i+3] = tmp8;
+                }
+            }
+
+        }
+    }
+}
+
+char solve_64_desc[] = "reference (in Chinese) for 64x64 solution: https://zhuanlan.zhihu.com/p/456858668";
+void solve_64(int M, int N, int A[N][M], int B[M][N])
+{
+    int i, j;
+
+    for (i = 0; i < N; i += 8) {      // 枚举每八行
+        for (j = 0; j < M; j += 8) {  // 枚举每八列
+            // 这里用这些临时变量，如果你查看过A和B的地址的话，你会发现A和B的地址差距是64的整数倍（0x40000），
+            // 那么 直接赋值的话，在对角线的时候 每一个Load A[i][i]紧跟Store B[i][i],将造成比较多的
+            // eviction
+            int temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, cnt;
+            // 1块8*8，我们分成4块来做，每一块4*4
+            // 这是 左上，且将左下的块移动到 B中的右上，这样是为了更高效地利用cache
+            for (cnt = 0; cnt < 4; ++cnt, ++i) {  // 枚举0~8中的每一行，一行八列
+                temp1 = A[i][j];  // 这样我们就一次取出了8个元素，我们的A的miss就只有一次了 原始4*4
+                                  // 则是两次
+                temp2 = A[i][j + 1];
+                temp3 = A[i][j + 2];
+                temp4 = A[i][j + 3];  // 左上
+
+                temp5 = A[i][j + 4];  // 右上
+                temp6 = A[i][j + 5];
+                temp7 = A[i][j + 6];
+                temp8 = A[i][j + 7];
+
+                B[j][i] = temp1;  // 左上翻转
+                B[j + 1][i] = temp2;
+                B[j + 2][i] = temp3;
+                B[j + 3][i] = temp4;
+
+                B[j][i + 4] = temp5;  //将A中右上 存到B中的右上，这也是全部命中的
+                B[j + 1][i + 4] = temp6;
+                B[j + 2][i + 4] = temp7;
+                B[j + 3][i + 4] = temp8;
+            }
+            i -= 4;
+            // 处理A中左下
+            for (cnt = 0; cnt < 4; ++j, ++cnt) {  // 枚举0~8中的每一行，一行八列
+                temp1 = A[i + 4][j];
+                temp2 = A[i + 5][j];
+                temp3 = A[i + 6][j];
+                temp4 = A[i + 7][j];  // 拿到左下的元素
+
+                // 因为我们这里本来就要处理 右上，所以这不会带来更多的miss
+                temp5 = B[j][i + 4];  // 拿到我们之前赋给B右上的，也就是A右上的元素
+                temp6 = B[j][i + 5];
+                temp7 = B[j][i + 6];
+                temp8 = B[j][i + 7];
+
+                B[j][i + 4] = temp1;  //将左下翻转到B右上
+                B[j][i + 5] = temp2;
+                B[j][i + 6] = temp3;
+                B[j][i + 7] = temp4;
+
+                // 这一步，也不会带来更多的miss因为 i + 4 <= j + 4 <= i + 7 恒成立
+                // 所以每次带来的 eviction 和 store B[j][i +
+                // 4]只会带来一次MISS，和原来的操作是一样的
+
+                B[j + 4][i] = temp5;  //将原B右上 赋值到 B左下， 这样A右上也就完成了翻转
+                B[j + 4][i + 1] = temp6;
+                B[j + 4][i + 2] = temp7;
+                B[j + 4][i + 3] = temp8;
+            }
+            j -= 4;
+            j += 4;  // 处理第四块 右下
+            for (i += 4, cnt = 0; cnt < 4; ++cnt, ++i) {
+                temp1 = A[i][j];  // 第四块没有任何改动， 和原来效果是一样的
+                temp2 = A[i][j + 1];
+                temp3 = A[i][j + 2];
+                temp4 = A[i][j + 3];
+
+                B[j][i] = temp1;
+                B[j + 1][i] = temp2;
+                B[j + 2][i] = temp3;
+                B[j + 3][i] = temp4;
+            }
+            i -= 8, j -= 4;
+        }
+    }
+}
+
+char transpose_61x67_desc[] = "perform transpose for 61x67 matrix by 17x17 blocks";
+void transpose_61x67(int M, int N, int A[N][M], int B[M][N]){
+    int i, j, k_row, k_col;
+    int block_row_size=17, block_col_size=17;
+
+    for(i=0; i<N; i+= block_row_size){
+        for(j=0; j<M; j+= block_col_size){
+            for(k_row=i; k_row<i+block_row_size && k_row<N; k_row++){
+                for(k_col=j; k_col<j+block_col_size && k_col<M; k_col++){
+                    B[k_col][k_row] = A[k_row][k_col];
+                }
+            }
+        }
+    }
+}
+
 /*
  * registerFunctions - This function registers your transpose
  *     functions with the driver.  At runtime, the driver will
@@ -59,8 +314,12 @@ void registerFunctions()
     registerTransFunction(transpose_submit, transpose_submit_desc); 
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
+    // registerTransFunction(trans, trans_desc); 
 
+    registerTransFunction(transpose_32x32, transpose_32x32_desc);
+    registerTransFunction(solve_64, solve_64_desc);
+    registerTransFunction(transpose_64x64, transpose_64x64_desc);
+    registerTransFunction(transpose_61x67, transpose_61x67_desc);
 }
 
 /* 
